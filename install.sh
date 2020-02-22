ea5b331a9daa254e0616dedaec123994d1e40deb
multiversecoder <pythonmultiverse@gmx.com>
2020-02-22 09:58:58 +0100

    Check NetworkManager on Install

--- install.sh
+++ install.sh
@@ -4,15 +4,22 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 echo "Installing darknet.py"
-echo "Checking Tor Requirement"
-if which tor; then
-    echo 'Tor is Installed'
-    echo "Installing darknet.py"
-    python3 ./setup.py install
-    echo "Sucessfully Installed!"
-    echo "Run `darknet.py --start --torid <YOUR_TOR_ID>` to start darknet.py!"
-    darknet.py -h
+echo "Checking NetworkManager Requirement"
+if which NetworkManager; then
+	echo "NetworkManager Installed"
+	echo "Checking Tor Requirement"
+	if which tor; then
+	    echo 'Tor is Installed'
+	    echo "Installing darknet.py"
+	    python3 ./setup.py install
+	    echo "Sucessfully Installed!"
+	    echo "Run darknet.py --start --torid <YOUR_TOR_ID> to start darknet.py!"
+	    darknet.py -h
+	else
+	    echo "Tor is not Installed"
+	    echo "Install Tor With your preferred package manager"
+	fi
 else
-    echo "Tor is not Installed"
-    echo "Install Tor With your preferred package manager"
+	echo "NetworkManager not found on this system."
+	echo "Install NetworkManager and retry the installation"
 fi
