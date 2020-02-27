f1c1e200c7f5866b893151481b0709ca44ababc7
Multiverse Kid <44272846+multiversecoder@users.noreply.github.com>
2020-02-28 00:00:36 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,20 @@
+import pathlib
 from setuptools import setup
 
-setup(name='darknet.py', 
-        author='Adriano Romanazzo',
-        author_email='pythonmultiverse@gmx.com',
-        scripts=['bin/darknet.py']
+README = f"{pathlib.Path(__file__).parent}/README.md"
+
+with open(README) as r:
+    README = r.read()
+
+setup(
+    name='darknet.py',
+    version='1.1',
+    author='Adriano Romanazzo (multiversecoder)',
+    description='darknet.py is a network application with no dependencies other than Python and TOR, useful to anonymize the traffic of linux servers and workstations.',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author_email='pythonmultiverse@gmx.com',
+    url='https://github.com/multiversecoder/darknet.py',
+    scripts=['bin/darknet.py'],
+    license='BSD 3-Clause License'
 )
